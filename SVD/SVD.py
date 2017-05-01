2aafc8e88ba88c006b1b0fb6f6173069206f3015
Adam Novotny <adam.novotny@postrock.wall>
2017-05-01 18:03:56 -0400

    Fixed calling svd_filter

--- SVD/SVD.py
+++ SVD/SVD.py
@@ -8,7 +8,7 @@ signal, noisy_signal = signal_noise(sn_ratio = 0.5)
 # noisy_signal = noisy_signal + (signal_list(signal_func=sin_signal) - .5)
 
 # Filter Signal
-svd_filtered = svd_filter(partition_vector(noisy_signal, 100), num_terms=1)
+svd_filtered = svd_filter(noisy_signal, window_size=100, num_terms=1)
 
 # Print mean squared errors
 print(mean_squared_error(signal, noisy_signal))

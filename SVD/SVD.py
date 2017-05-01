a91f16b37611cf2fbbcb1c0bdfcffe4c76141275
Adam Novotny <adam.novotny@postrock.wall>
2017-05-01 19:18:05 -0400

    Added cut partitioning

--- SVD/SVD.py
+++ SVD/SVD.py
@@ -2,17 +2,19 @@ from svd_functions import *
 
 
 # Create Signal
-signal, noisy_signal = signal_noise(sn_ratio = 0.5)
+signal, noisy_signal = signal_noise(sn_ratio = 1)
 
 # Add sin wave to signal
 # noisy_signal = noisy_signal + (signal_list(signal_func=sin_signal) - .5)
 
 # Filter Signal
-svd_filtered = svd_filter(noisy_signal, window_size=100, num_terms=1)
+svd_filtered_cut = svd_filter(noisy_signal, partition_type="cut")
+svd_filtered_sliding = svd_filter(noisy_signal, partition_type="sliding")
 
 # Print mean squared errors
-print(mean_squared_error(signal, noisy_signal))
-print(mean_squared_error(signal, svd_filtered))
+print("Noisy Signal:\t", mean_squared_error(signal, noisy_signal))
+print("SVD Cut:\t", mean_squared_error(signal, svd_filtered_cut))
+print("SVD Sliding:\t",mean_squared_error(signal, svd_filtered_sliding))
 
 # Plot all vectors
-plot_vector([noisy_signal, svd_filtered, signal], trace_labels=['noisy', 'filtered', 'original'])
+plot_vector([noisy_signal, svd_filtered_cut, svd_filtered_sliding, signal], trace_labels=['noisy', 'filtered_cut', 'filtered_sliding', 'original'])
--- SVD/svd_functions.py
+++ SVD/svd_functions.py
@@ -5,20 +5,23 @@ import random
 
 
 LENGTH_MS = 1000;
-SAMPLING_FREQ = 1000;
+SAMPLING_FREQ = 2000;
+
+WINDOW_SIZE = 10
+NUM_TERMS = 1
 
 
 def signal_function(t):
     """ Returns values to be used as a signal.
     """
-    return square_signal(t)
-    # return sin_signal(t)
+    # return square_signal(t)
+    return sin_signal(t)
 
 def sin_signal(t):
     """ Returns values of many sin waves.
     """
-    # return np.sin(50*t) + np.sin(20*t) + np.sin(12*t) + np.sin(60*t)
-    return sin(10*t)
+    # return sin(10*t)
+    return np.sin(50*t) + np.sin(20*t) + np.sin(12*t) + np.sin(60*t)
 
 def square_signal(t, clock_ms=100, sampling_freq=SAMPLING_FREQ):
     """ Returns a square signal.
@@ -44,10 +47,12 @@ def signal_list(length_ms=LENGTH_MS, sampling_freq=SAMPLING_FREQ, signal_func=No
     signal_norm = signal/max(signal)
     return signal_norm
 
+
 def noise(length):
     """ Returns a list of random values from -1 to 1.
     """
     return (np.random.random(length) * 2) - 1
+
 
 def signal_noise(sn_ratio = 1, length_ms=LENGTH_MS, sampling_freq=SAMPLING_FREQ):
     """ Returns two lists, clean signal and signal with noise.
@@ -55,6 +60,7 @@ def signal_noise(sn_ratio = 1, length_ms=LENGTH_MS, sampling_freq=SAMPLING_FREQ)
     signal = signal_list(length_ms, sampling_freq)
     noisy_signal = signal + (noise(len(signal)) * sn_ratio)
     return signal, noisy_signal
+
 
 def plot_vector(wave, downsample=1, title="", ax_labels=["",""], trace_labels = [""]):
     """ Makes a plot of a vector or list of vectors.
@@ -75,21 +81,24 @@ def plot_vector(wave, downsample=1, title="", ax_labels=["",""], trace_labels = 
     ax.set_ylabel(ax_labels[1])
     plt.show()
 
+
 def mean_squared_error(a, b):
     """ Compares two lists with MSE.
     """
     len_to_compare = min(len(a), len(b))
     return ((a[:len_to_compare] - b[:len_to_compare])**2).mean(axis=None)
 
-def partition_vector(input_vector, window_size, step_size=1):
+
+def partition_vector_sliding(input_vector, window_size, step_size=1):
     """ Partitions a vector into a matrix by sliding a window of window size over it, with a step of step_size.
-        Cuts off non-divisible values of input_vector.
+        Pads input_vector with last value of input vector.
     """
     if(len(input_vector) < window_size):
         raise ValueError("Input vecor is smaller than window size", len(input_vector, window_size))
 
-    x_size = int((len(input_vector) - window_size) / step_size) - 1
-    print(len(input_vector)/step_size, x_size)
+    input_vector = np.append(input_vector, [input_vector[-1]] * (window_size))
+    x_size = int((len(input_vector) - window_size) / step_size) + 1
+    # print(len(input_vector)/step_size, x_size)
 
     out_matrix = np.zeros([x_size, window_size])
     for i in range(x_size):
@@ -99,12 +108,42 @@ def partition_vector(input_vector, window_size, step_size=1):
     return out_matrix
 
 
-def svd_filter(input_vector, window_size, step_size=1, num_terms=1):
+def unpartition_matrix_sliding(input_matrix, step_size=1):
+    """ Returns a vector from a matrix made with 'sliding'
+    """
+    out_vector = np.append(input_matrix[:-1,:step_size], input_matrix[-1, :])
+    print(max(out_vector))
+    return out_vector
+
+
+def partition_vector_cut(input_vector, window_size):
+    """ Cuts a vector up into a matrix without overlapping. Discards undivisible data.
+    """
+    if(len(input_vector) < window_size):
+        raise ValueError("Input vecor is smaller than window size", len(input_vector, window_size))
+
+    x_size = int(len(input_vector) / window_size)
+    out_matrix = np.zeros([x_size, window_size])
+    for i in range(x_size):
+        out_matrix[i] = input_vector[i*window_size : (i*window_size)+window_size]
+
+    return out_matrix
+
+def unpartition_matrix_cut(input_matrix):
+    """ Returns a vector from a matrix made with 'cut'
+    """
+    out_vector = input_matrix.flatten()
+    return out_vector
+
+
+def svd_filter(input_vector, window_size=WINDOW_SIZE, num_terms=NUM_TERMS, step_size=1, partition_type="cut"):
     """ Returns a vector filtered by svd. It partitions the input, takes num_terms largest svd values,
         then unpartitions the resulting matrix into a filtered vector.
     """
-
-    svd_input = partition_vector(input_vector, window_size, step_size)
+    if partition_type == "cut":
+        svd_input = partition_vector_cut(input_vector, window_size)
+    elif partition_type ==  "sliding":
+        svd_input = partition_vector_sliding(input_vector, window_size, step_size)
 
     # print(svd_input, "\n")
     U, s, V = np.linalg.svd(svd_input)
@@ -128,6 +167,10 @@ def svd_filter(input_vector, window_size, step_size=1, num_terms=1):
 
     # print(output_matrix)
 
-    output_vector = np.append(output_matrix[:-1,0], output_matrix[-1, :])
+    if partition_type == "cut":
+        output_vector = unpartition_matrix_cut(output_matrix)
+    if partition_type == "sliding":
+        output_vector = unpartition_matrix_sliding(output_matrix, step_size)[:len(input_vector)]
+        # output_vector = output_vector / max(output_vector)
 
     return output_vector
